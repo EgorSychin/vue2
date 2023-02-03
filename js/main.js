880a74f081c9685c736c0ae42744f1f8ced2ab47
Egor <egorsychin@mail.ru>
2023-02-03 08:47:25 +0700

    line-through

--- js/main.js
+++ js/main.js
@@ -4,34 +4,34 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
+        inStock: false,
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
-        inStock: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'Green',
+                variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
             },
             {
                 variantId: 2235,
-                variantColor: 'Blue',
+                variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
         ],
-        cart: 0,
+        cart: 0
+    },
         methods: {
             addToCart() {
                 this.cart += 1
+            },
+            updateProduct(variantImage) {
+                this.image = variantImage
             }
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
-        }
-
-    }
 })
+
 
 
 
